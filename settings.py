4e82848c90ae0769ce1b4e2f7ef9cd8dc4dee1dd
egor.kochergin <egor.kochergin@aisa.ru>
2022-07-15 02:43:31 +0300

    Init project

---
+++ settings.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = 'REPLACE_ME'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
